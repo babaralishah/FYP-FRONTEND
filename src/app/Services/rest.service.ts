e3ed6aba03b260b8ee9b3bd6475cc46cc5190eff
babaralishah <babar.bscs3261@iiu.edu.pk>
2020-10-17 16:10:16 +0500

    first commit

--- src/app/Services/rest.service.ts
+++ src/app/Services/rest.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams, HttpEvent, HttpErrorResponse, HttpEventType } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,15 @@ export class RestService {
   constructor(
     private http: HttpClient) { }
 
+  url = 'https://fyp-prediction-backend.herokuapp.com';
   backendAddress = 'http://127.0.0.1:5000/output/';
   backendAddress1 = 'http://127.0.0.1:5000/output1/';
   backendAddress2 = 'http://127.0.0.1:5000/output2/';
 
   // tslint:disable-next-line: typedef
   readResults() {
-    return this.http.get(this.backendAddress1);
+    // return this.http.get(this.backendAddress1);
+    return this.http.get(`${this.url}/output1`);
   }
 
   // tslint:disable-next-line: typedef
@@ -24,7 +26,8 @@ export class RestService {
     const formData: FormData = new FormData();
     formData.append('name1', credentials.name1);
     formData.append('name2', credentials.name2);
-    return this.http.post(this.backendAddress2, formData);
+    // return this.http.post(this.backendAddress2, formData);
+    return this.http.get(`${this.url}/output2`);
   }
 
   // tslint:disable-next-line: typedef
@@ -32,7 +35,8 @@ export class RestService {
     const formData: FormData = new FormData();
     formData.append('file', fileToUpload, fileToUpload.name);
     console.log('File Name: ', fileToUpload.name); // and File and its name: \' ' + fileToUpload + fileToUpload.name);
-    return this.http.post(this.backendAddress, formData); // ,{  responseType: 'arraybuffer' | 'blob' | 'json' | 'text' });
+    // return this.http.post(this.backendAddress, formData); // ,{  responseType: 'arraybuffer' | 'blob' | 'json' | 'text' });
+    return this.http.get(`${this.url}/output`);
   }
 
 }
